25445f9710180531c25f86c853864b9e39e75aa3
BokarevDmitry <dbokarevv@gmail.com>
2019-11-30 00:00:50 +0300

    Scripted auto-commit on change (2019-11-30 00:00:50) by gitwatch.sh

--- src/main/java/bokarev/App.java
+++ src/main/java/bokarev/App.java
@@ -37,7 +37,7 @@ import io.netty.handler.codec.http.*;
 
 public class App extends AllDirectives {
     public static void main(String[] args) throws Exception, InterruptedException, IOException {
-        /*ActorSystem system = ActorSystem.create("routes");
+        ActorSystem system = ActorSystem.create("routes");
         ActorRef routerActor = system.actorOf(bokarev.RouterActor.props(system), "Router-Actor");
 
         final Http http = Http.get(system);
@@ -49,7 +49,7 @@ public class App extends AllDirectives {
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
-        );*/
+        );
 
         System.out.println("Server online at http://localhost:8080/");
         AsyncHttpClient asyncHttpClient = asyncHttpClient();

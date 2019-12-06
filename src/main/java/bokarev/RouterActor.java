b74b6c9470f6b813b0f90c8f7db07946ef18737e
BokarevDmitry <dbokarevv@gmail.com>
2019-12-06 18:03:57 +0300

    Scripted auto-commit on change (2019-12-06 18:03:57) by gitwatch.sh

--- src/main/java/bokarev/RouterActor.java
+++ src/main/java/bokarev/RouterActor.java
@@ -30,9 +30,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.regex.Pattern;
 
 
-public class RouterActor extends AbstractActor {
-    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
-
+public class RouterActor {
     ActorMaterializer actorMaterializer;
     AsyncHttpClient asyncHttpClient;
     ActorRef storageActor;
@@ -42,44 +40,6 @@ public class RouterActor extends AbstractActor {
         this.asyncHttpClient = asyncHttpClient;
         this.storageActor = system.actorOf (StorageActor.props(), "Storage-Actor");
     }
-
-    public static Props props(ActorSystem system) {
-        return Props.create(RouterActor.class, system);
-    }
-
-    @Override
-    public void preStart() {
-        log.info("Starting RouterActor {}", this.getSelf());
-    }
-
-    @Override
-    public void postStop() {
-        log.info("Stopping RouterActor {}", this.getSelf());
-    }
-
-    @Override
-    public Receive createReceive() {
-        return receiveBuilder()
-
-                .match(UrlWithCount.class, msg -> {
-                    //Flow<UrlWithCount, HttpResponse, NotUsed> flow = Flow.of(UrlWithCount.class)
-                            //.map(req -> new Pair<String, Integer>(req.getUrl(), req.getCount()))
-                            //.mapAsync()
-                    //storageActor.tell(msg, getSelf());
-                    //Patterns.ask(storageActor)
-                    //Sink<UrlWithCount, CompletionStage<Long>> testSink = Sink
-
-                })
-                .match(TestWithResult.class, msg -> {
-                    ActorRef testPasserActor = getContext().actorOf(TestPasserActor.props(), "TestPasser-Actor");
-                    testPasserActor.tell(msg, getSender());
-                })
-
-                .build();
-    }
-
-
-
 
     Flow<HttpRequest, HttpResponse, NotUsed> createRoute() {
         return Flow.of(HttpRequest.class)

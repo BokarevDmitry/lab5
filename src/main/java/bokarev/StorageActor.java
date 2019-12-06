e50d00c88ffd9ed00305fafae4b8fbbd82ca015e
BokarevDmitry <dbokarevv@gmail.com>
2019-12-06 18:48:02 +0300

    Scripted auto-commit on change (2019-12-06 18:48:02) by gitwatch.sh

--- src/main/java/bokarev/StorageActor.java
+++ src/main/java/bokarev/StorageActor.java
@@ -40,7 +40,7 @@ public class StorageActor extends AbstractActor {
                 })
 
                 .match(UrlWithCount.class, r -> {
-                    log.info("REQUEST: tests for package - " + r.getUrl());
+                    log.info("REQUEST: tests for package - " + r.getUrl() + " " + r.getCount());
                     log.info(testResults.get(r).toString());
                     //getSender().tell(new TestWithResult(r, testResults.get(r)), ActorRef.noSender());
                 })
